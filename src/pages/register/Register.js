0258535f730c10a30a3dad6730914b0980eb1131
blessey15 <blesseymariasaju@gmail.com>
2022-06-27 09:50:13 +0530

    Register Page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -4,9 +4,30 @@ import styles from './Register.module.scss'
 class Register extends React.Component {
     render() {
         return (
-            <div className={styles.container}>
-                <p>slkdfjlkj</p>
+            <div className="container">
+            <div className="register-box">
+                <h2 className="register-title">Register</h2>
+                <h4 className="register-subtitle">Already registered? <a href="login.html">Login</a></h4>
+                <div className="register-form">
+                <form className={styles.form}>
+                <label>First Name</label>
+                <input type="text"/>
+                <label>Last Name</label>
+                <input type="text"/>
+                <label>Email Address</label>
+                <input type="email"/>
+                <label>Password</label>
+                <input type="password"/>
+                <p>Forgot Password?</p>
+                <button>Login</button>
+                <p>Don't have an account? <Link to='/register'>Register</Link></p>
+            </form>
+                </div>
             </div>
+            <div class="sidebox">
+                <img class="sideimg" src="img/4063097.jpg"/>
+            </div>
+        </div>
         )
     }
 }
