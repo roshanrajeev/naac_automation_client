8cf96620badaddf67e8b3f455b186804eaf1a61b
blessey15 <blesseymariasaju@gmail.com>
2022-06-30 22:26:41 +0530

    created login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Login.module.scss'
-
+import bgImage from '../../assets/img/bg-register.jpg'
 
 class Login extends Component {
   render() {
@@ -12,16 +12,28 @@ class Login extends Component {
             <p className={styles.subtitle}>Please login to your account</p>
 
             <form className={styles.form}>
-                <label>Email Address</label>
-                <input type="email"/>
-                <label>Password</label>
-                <input type="password"/>
-                <p>Forgot Password?</p>
-                <button>Login</button>
-                <p>Don't have an account? <Link to='/register'>Register</Link></p>
+                <label className={styles.name}>Email Address</label>
+                <input className={styles.inputstyle} type="email"/>
+                <label className={styles.name}>Password</label>
+                <input className={styles.inputstyle} type="password"/>
+                <div className={styles.subsection}>
+                  {/* <div className={styles.rememberme}>
+                    <div className={styles.square}></div>
+                    <p className={styles.rm}>Remember me</p>
+                  </div> */}
+                  <div>
+                  <input type="checkbox" className={styles.square} name="Remember Me" value="yes" id="remember"/>
+                  <label for="remember" className={styles.rm} >Remember me</label><br></br>
+                  </div>
+                <p className={styles.forgetpassword}>Forgot Password?</p>
+                </div>
+                <button className={styles.login_button}>Login</button>
             </form>
+            <p className={styles.register}>Don't have an account? <Link to='/register'>Register</Link></p>
         </div>
         <div className={styles.imageContainer}>
+                <img className={styles.sideimg} src={bgImage} alt=""/>
+                <h6 className={styles.welcome_back}>Welcome Back</h6>
         </div>
       </div>
     )
