0655e2022cce48ec8afe7cff2beecf5e77ebd3cb
blessey15 <blesseymariasaju@gmail.com>
2022-07-18 09:41:22 +0530

    redesigning register page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,50 +1,38 @@
 import React from 'react'
 import styles from './Register.module.scss'
-//import bgImage from '../../assets/img/bg-register.jpg'
 import {Link} from 'react-router-dom'
 import Nav from '../navbar/Navbar'
 class Register extends React.Component {
     render() {
         return (
-              <Nav/>
-            // <div className={styles.container}>
-            //     <div className={styles.register_box}>
-            //             <form className={styles.form}>
-            //             <h2 className={styles.register_title}>REGISTER</h2>
-            //                 <div className={styles.nameinput}>
-            //                     <div className={styles.firstname}>
-            //                         <label className={styles.name}>First Name</label>
-            //                         <input className={styles.inputstyle} type="text"/>
-            //                     </div>
-
-            //                     <div className={styles.lastname}>
-            //                         <label className={styles.name}>Last Name</label>
-            //                         <input className={styles.inputstyle} type="text"/>
-            //                     </div>
-            //                 </div>
-       
-            //                 <label className={styles.name}>Email Address</label>
-            //                 <input className={styles.inputstyle} type="email"/>
-                           
-            //                 <label className={styles.name}>Institution</label>
-            //                 <input className={styles.inputstyle} type="text"/>
-                           
-            //                 <label className={styles.name}>Designation</label>
-            //                 <input className={styles.inputstyle} type="text"/>
-            //                 <label className={styles.name}>Department</label>
-            //                 <input className={styles.inputstyle} type="text"/>
-                           
-            //                 <label className={styles.name}>Password</label>
-            //                 <input className={styles.inputstyle} type="password"/>
-                           
-            //                 <div className={styles.register_submit}>
-            //                     <button className={styles.register_button}>Register</button>
-            //                 </div>
-            //                 <h4 className={styles.register_subtitle}>Already registered? <Link to='/login'>Login</Link></h4>
-            //             </form>      
-            //         {/* </div> */}
-            //     </div>
-            // </div>
+            <div className={styles.container}>
+                <Nav/>
+                <div className={styles.register_box}>
+                    <h1 className={styles.register_text}>Register</h1>
+                    <form className={styles.register_form}>
+                        <div className={styles.name}>
+                            <div className={styles.firstname}>
+                                <label className={styles.label}>First Name</label>
+                                <input className={styles.inputstyle} type="text"/>
+                            </div>
+                            <div className={styles.lastname}>
+                                <label className={styles.label}>Last Name</label>
+                                <input className={styles.inputstyle} type="text"/>
+                            </div>
+                        </div>
+                        {/* Email */}
+                        <label className={styles.label}>Email</label>
+                        <input className={styles.inputstyle} type="email"/>
+                        {/* Password */}
+                        <label className={styles.label}>Password</label>
+                        <input className={styles.inputstyle} type="password"/>
+                        {/* Confirm Password */}
+                        <label className={styles.label}>Confirm Password</label>
+                        <input className={styles.inputstyle} type="password"/>
+                    </form>
+                    <button className={styles.submit_button} type='submit'></button>
+                </div>
+            </div>
         )
     }
 }
