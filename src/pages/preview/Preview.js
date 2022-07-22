418bc9ec67f4017b1b9475f10b249e7396184146
roshanrajeev <roshanrajeev2001@gmail.com>
2022-07-22 17:11:00 +0530

    Preview

--- src/pages/preview/Preview.js
+++ src/pages/preview/Preview.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import Header from '../../components/header/Header'
 import toDocx from '../../utils/toDocx'
 import styles from "./Preview.module.scss"
+import withRouter from '../../utils/withRouter'
+import { renderAsync } from 'docx-preview'
+import Button from '../../components/button/Button'
+import variables from "../../assets/styles/mixins.scss"
+import "./Preview.scss"
 
 export class Preview extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            url: ""
+            data: ""
         }
+        this.preview = createRef()
     }
     componentDidMount() {
-        // const data = window.localStorage.getItem()
-        // toDocx()
-        console.log(this.props.match)
+        const asyncFunc = async (sections) => {
+            const blob = await toDocx(sections)
+            await renderAsync(blob, this.preview.current, this.preview.current)
+        }
+        const {id} = this.props.params
+        const json = window.localStorage.getItem(id)
+        if(!json) return
+        const sections = JSON.parse(json)
+        asyncFunc(sections)
     }
   render() {
     return (
       <div>
-        <Header/>
-        
+        <div className={styles.headerContainer}>
+            <Header/>
+        </div>
+        <div className='container'>
+            <div className={styles.topBarContainer}>
+                <h1 className={styles.title}>Main Heading</h1>
+                <Button type="filled" color={variables.color_yellow} size="small">Download</Button>
+            </div>
+            <div ref={this.preview} className={styles.preview}></div>
+        </div>
       </div>
     )
   }
 }
 
-export default Preview
+export default withRouter(Preview)
